f74559b4ba234c9c940ce58d2e72124eaece6fb9
Fritz <fritz.bewersdorf@gmx.de>
2020-04-04 22:46:39 +0200

    Perfekt

--- Nutzerverwaltung_Admin.js
+++ Nutzerverwaltung_Admin.js
@@ -5,9 +5,55 @@ Name123 = document.getElementById("NameNehmen");
 
 //DozentenAnlegenListe
 /*
-document.getElementById("neuerdozent").addEventListener('click', function(){
-
-})
+function dialogNeueListe()
+{
+    dialogNeueEinkaufsListe.showModal();
+}
+ 
+function listeHinzugefügt()
+{
+    var listId = document.getElementById("neueListenID").value;
+ 
+    listeAnheften(listId);
+    getListName(listId);
+    document.getElementById("neueListenID").value = "";
+    dialogNeueEinkaufsListe.close();
+}
+ 
+function getListName (id)
+{
+    var listId = id;
+    var apiUrl = "https://shopping-lists-api.herokuapp.com/api/v1/lists/" + listId;
+ 
+    fetch(apiUrl, {Method: "GET"})
+        .then(response => response.json())
+        .then(listName)
+        .catch(err => console.error(err));
+}
+ 
+function listName (liste)
+{    
+    localStorage.setItem(liste._id, liste.name);
+    
+    listeAktualisieren();
+}
+ 
+function listeAbgebrochen ()
+{
+    dialogNeueEinkaufsListe.close();
+}
+ 
+function listeAnheften(listID)
+{
+    
+    var apiUrl = "https://shopping-lists-api.herokuapp.com/api/v1/lists/" + listID;
+ 
+    fetch(apiUrl, {Method: "GET"})
+    .then(response => response.json())
+    .then(showList)
+    .catch(err => console.error(err));
+    
+}
 */
 
 //NeuenDozentAnlegen
@@ -53,7 +99,7 @@ document.getElementById("Button1").addEventListener('click', function() {
 //DozentenGenerieren
 getData();
 
-   async function getData(){
+  async function getData(){
     clearContent();
     const response = await fetch('http://localhost:8080/Nutzer/0');
     const data = await response.json();
@@ -63,15 +109,33 @@ getData();
     for (item of data)
         {
           if(item.status.staId==2){
-            var listitem = document.createElement("div"); 
+            var listitem = document.createElement("li"); 
             listitem.textContent = "Herr/Frau " + item.nutNachname;
-            //document.getElementById("dozenteinzeln").appendChild(listitem);
+            listitem.setAttribute('class', 'shadow p-3 mb-5 bg-white');
+            
+            var divi = document.createElement("div");
+            divi.setAttribute('class', 'btn-group');
+
+            var button1234 = document.createElement("button");
+            button1234.setAttribute('type', 'button');
+            button1234.setAttribute('class', 'btn dropdown-toggle');
+            button1234.setAttribute( 'data-toggle', 'dropdown');
+            button1234.setAttribute('aria-expanded', 'false');
+
+            var divi2 = document.createElement("div");
+            divi2.setAttribute('class', 'dropdown-menu');
+
+            var drueck = document.createElement("a");
+            drueck.textContent = "Dozent löschen";
+            drueck.setAttribute('class', 'dropdown-item');
+            /*
             var Knopf = document.createElement("button");
             var imageButtoncheck = new Image(15, 15);
               imageButtoncheck.src = "Trash.jpg";
               Knopf.appendChild(imageButtoncheck);
+            */
 
-          Knopf.addEventListener('click',function deletos()
+          drueck.addEventListener('click',function deletos()
           {
               var apiUrl2 = "http://localhost:8080/Nutzer/" + item.nutId ;
               fetch(apiUrl2, {method: 'DELETE',
@@ -83,15 +147,15 @@ getData();
                   location.reload();
 
           });
-          listitem.appendChild(Knopf);
+          listitem.appendChild(divi);
+          divi.appendChild(button1234);
+          divi.appendChild(divi2);
+          divi2.appendChild(drueck);
           ul.appendChild(listitem);
         }
-        else{
-
-        }
-       }
-       document.getElementById("ungeordneteListe").appendChild(ul);
-      };
+      }
+      document.getElementById("ungeordneteListe").appendChild(ul);
+  };
 
 function clearContent()
 {
